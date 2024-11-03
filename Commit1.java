4e9c194a357562041b006d00f24f2fb141faf21c
Nguiramto <douglasmbainaissem05@gmail.com>
2024-11-03 16:11:09 +0300

    Update Commit1
    A constructor has been added to Commit1 to initialized the two instances variables.

--- Commit1.java
+++ Commit1.java
@@ -3,4 +3,10 @@ public class Commit1 {
     private String name;
     private int phoneNumber;
     
+    public Commit1(String name, int phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+
 }
